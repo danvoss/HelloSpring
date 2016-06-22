e943fd4a58092e8d1013b2478906181ef6d77c11
Dan <danielandrewvoss@gmail.com>
2016-06-22 11:02:45 -0400

    Add json routes

---
+++ src/main/java/com/dvoss/HelloSpringJsonController.java
@@ -0,0 +1,23 @@
+package com.dvoss;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Dan on 6/22/16.
+ */
+@RestController
+public class HelloSpringJsonController {
+    @RequestMapping(path = "/person.json", method = RequestMethod.GET)
+    public Person getPerson() {
+        Person p = new Person("Alice", "Charleston", 40);
+        return p;
+    }
+
+    @RequestMapping(path = "/person.json", method = RequestMethod.POST)
+    public void addPerson(@RequestBody Person person) { // can only be one request body per route, but could be a hashmap instead of person obj
+        System.out.println("Received: " + person.name);
+    }
+}
